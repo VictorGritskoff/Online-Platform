ff7057a111a24da8e99cd89b5bace440f9403ae7
Victor Gritskoff <143332387+VictorGritskoff@users.noreply.github.com>
2024-05-15 09:47:43 +0300

    Update EmployeeService.java

--- services/EmployeeService.java
+++ services/EmployeeService.java
@@ -2,34 +2,3 @@ package com.example.example.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class CourseService {
-
-    @Autowired
-    private CourseRepository courseRepository;
-
-   
-    public List<Course> getCoursesWithTeachers() {
-        return courseRepository.findAllWithTeacher();
-    }
-
-
-    public Course getCourseById(int id) {
-        return courseRepository.findById(id).orElse(null);
-    }
-
-    public void addCourse(Course course) {
-        courseRepository.save(course);
-    }
-
-    public void updateCourse(int id, Course updatedCourse) {
-        if (courseRepository.existsById(id)) {
-            updatedCourse.setId(id);
-            courseRepository.save(updatedCourse);
-        }
-    }
-
-    public void deleteCourse(int id) {
-        courseRepository.deleteById(id);
-    }
-}

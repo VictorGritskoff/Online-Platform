6e82a32fdf701f9b1555ecd5b9debbb5bb1f198a
Victor Gritskoff <143332387+VictorGritskoff@users.noreply.github.com>
2024-05-15 09:50:32 +0300

    Update TeacherService.java

--- services/TeacherService.java
+++ services/TeacherService.java
@@ -10,7 +10,31 @@ import java.util.List;
 
 @Service
 public class TeacherService {
+
     @Autowired
     private TeacherRepository teacherRepository;
 
+    public List<Teacher> getAllTeachers() {
+        return teacherRepository.findAll();
+    }
+
+    public Teacher getTeacherById(int id) {
+        Optional<Teacher> teacher = teacherRepository.findById(id);
+        return teacher.orElse(null);
+    }
+
+    public void addTeacher(Teacher teacher) {
+        teacherRepository.save(teacher);
+    }
+
+    public void updateTeacher(int id, Teacher updatedTeacher) {
+        if (teacherRepository.existsById(id)) {
+            updatedTeacher.setId(id);
+            teacherRepository.save(updatedTeacher);
+        }
+    }
+
+    public void deleteTeacher(int id) {
+        teacherRepository.deleteById(id);
+    }
 }

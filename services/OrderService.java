31a4d0f11ce3c2a9540a41d8a47598abed93b8a7
Victor Gritskoff <143332387+VictorGritskoff@users.noreply.github.com>
2024-05-15 09:49:38 +0300

    Update OrderService.java

--- services/OrderService.java
+++ services/OrderService.java
@@ -4,4 +4,34 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Order getOrderById(int id) {
+        Optional<Order> order = orderRepository.findById(id);
+        return order.orElse(null);
+    }
+
+    public List<Order> getOrdersByClientId(int clientId) {
+        return orderRepository.findByClientId(clientId);
+    }
+
+    public void addOrder(Order order) {
+        orderRepository.save(order);
+    }
+
+    public void updateOrder(int id, Order updatedOrder) {
+        if (orderRepository.existsById(id)) {
+            updatedOrder.setId(id);
+            orderRepository.save(updatedOrder);
+        }
+    }
+    public void deleteOrder(int id) {
+        orderRepository.deleteById(id);
+    }
 }
